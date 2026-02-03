d8f315bf9cdecb0d3444fbc095b49cc089b7f76f
Nandni Srivastava <nandni.srivastava@grofers.com>
2026-02-03 09:26:54 +0530

    Reverse list using 3 ways

---
+++ lists/reverse_list.py
@@ -0,0 +1,29 @@
+def reverse(lst):
+    
+    # # Using in place method
+    # lst.reverse()
+    # return lst
+
+    # # Using slicing
+    # return lst[::-1]
+
+
+    # Using loop
+    start = 0
+    end = len(lst) - 1
+
+    while start < end:
+        lst[start], lst[end] = lst[end], lst[start]
+        start += 1
+        end -= 1
+
+    return lst
+
+
+
+if __name__ == "__main__":
+    # Input: space-separated integers
+    arr = list(map(int, input("Enter list elements separated by space: ").split()))
+    
+    result = reverse(arr)
+    print("List after reversal:", result)
